1ef873dfcd8f073eaf9e046d9f6fa2766e9f04a5
Valdemir Junior <valdemirvoltatoni@gmail.com>
2023-12-31 12:42:41 -0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, BodyComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, BodyComponent
+  ,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
